d503b30e0151d012054e8e21851a38c065e0da05
Artidoro Pagnoni <pagnoni.artidoro@gmail.com>
2023-07-07 16:33:26 -0700

    fixed a few bugs with API

--- qlora-submission/main.py
+++ qlora-submission/main.py
@@ -35,26 +35,13 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-adapter_path = get_last_checkpoint('qlora/output/guanaco-7b')
+adapter_path, _ = get_last_checkpoint('qlora/output/guanaco-7b')
+# adapter_path = 'timdettmers/guanaco-7b' # If you want to skip training, you can jsut load this model from the Hub.
 model_name_or_path = 'huggyllama/llama-7b'
 
 t0 = time.time()
 logger.info("Loading model ...")
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_name_or_path,
-#     load_in_4bit=bits == 4,
-#     device_map=device_map,
-#     max_memory=max_memory,
-#     quantization_config=BitsAndBytesConfig(
-#         load_in_4bit=bits == 4,
-#         llm_int8_threshold=6.0,
-#         llm_int8_has_fp16_weight=False,
-#         bnb_4bit_compute_dtype=torch.bfloat16,
-#         bnb_4bit_use_double_quant=True,
-#         bnb_4bit_quant_type='nf4',
-#     ),
-#     torch_dtype=torch.bfloat16,
-# )
+
 # Load the model (use bf16 for faster inference)
 model = AutoModelForCausalLM.from_pretrained(
     model_name_or_path,
@@ -67,16 +54,13 @@ logger.info(f"Time to load model: {time.time() - t0:.02f} seconds.")
 
 model.eval()
 
-tokenizer = AutoTokenizer(model_name_or_path)
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 tokenizer.bos_token_id = 1
 
 @app.post("/process")
 async def process_request(input_data: ProcessRequest) -> ProcessResponse:
     inputs = tokenizer(input_data.prompt, return_tensors="pt").to('cuda')
 
-
-    prompt_length = encoded.size(0)
-    # for i in range(input_data.num_samples):
     t0 = time.perf_counter()
 
     outputs = model.generate(
@@ -90,11 +74,11 @@ async def process_request(input_data: ProcessRequest) -> ProcessResponse:
             return_dict_in_generate=True,
         )
     )
-    
+    import pdb; pdb.set_trace()
     t = time.perf_counter() - t0
     
     # Extract tokens
-    generated_ids = outputs.sequences[0][len(outputs.scores):]
+    generated_ids = outputs.sequences[0][-len(outputs.scores):]
     text = tokenizer.decode(generated_ids, skip_special_tokens=True)
     tokens = tokenizer.convert_ids_to_tokens(generated_ids, skip_special_tokens=True)
 
@@ -113,24 +97,24 @@ async def process_request(input_data: ProcessRequest) -> ProcessResponse:
         f"Time for inference: {t:.02f} sec total, {tokens_generated / t:.02f} tokens/sec"
     )
     logger.info(f"Memory used: {torch.cuda.max_memory_reserved() / 1e9:.02f} GB")
-    tokens = []
-    for t, lp, tlp in zip(tokens, logprobs, top_logprobs):
+    ret_tokens = []
+    for tok, lp, tlp in zip(tokens, logprobs, top_logprobs):
         idx, val = tlp
         tok_str = tokenizer.decode(idx)
-        token_tlp = {tok_str: val}
-        tokens.append(
-            Token(text=t, logprob=lp, top_logprob=token_tlp)
+        token_tlp = {tok_str: float(val.item())}
+        ret_tokens.append(
+            Token(text=tok, logprob=float(lp.item()), top_logprob=token_tlp)
         )
     logprobs_sum = sum(logprobs)
     # Process the input data here
     return ProcessResponse(
-        text=text, tokens=tokens, logprob=logprobs_sum, request_time=t
+        text=text, tokens=ret_tokens, logprob=logprobs_sum, request_time=t
     )
 
 
 @app.post("/tokenize")
 async def tokenize(input_data: TokenizeRequest) -> TokenizeResponse:
     t0 = time.perf_counter()
-    inputs = tokenizer(input_data.prompt)
+    inputs = tokenizer(input_data.text, max_length=input_data.max_length, truncation=input_data.truncation)
     t = time.perf_counter() - t0
     return TokenizeResponse(tokens=inputs.input_ids, request_time=t)
