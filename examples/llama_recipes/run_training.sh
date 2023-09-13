8d945eab78f7216bd227669626e8e73154dce57c
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-13 11:23:30 +0000

    Inline training command into docker CMD

--- examples/llama_recipes/run_training.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-huggingface-cli login --token $HUGGINGFACE_TOKEN
-
-python3 -m llama_recipes.finetuning  --model_name meta-llama/Llama-2-7b --use_peft --peft_method lora --quantization --batch_size_training 2 --dataset custom_dataset --custom_dataset.file /workspace/custom_dataset.py --output_dir /volume/output_dir
